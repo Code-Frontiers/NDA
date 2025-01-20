3774b0762162ffc4483d86e0da93c2f174869d51
Shubham Mondal <shubhamank002@gmail.com>
2025-01-21 00:26:13 +0530

    update home page

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,22 +1,25 @@
-import React, { useRef } from 'react'
-import './VideoPlayer.css'
-import video from '../../assets/college-video.mp4'
+import React, { useRef } from "react";
+import "./VideoPlayer.css";
+import video from "../../assets/college-video.mp4";
 
-const VideoPlayer = ({playState, setPlayState}) => {
-
+const VideoPlayer = ({ playState, setPlayState }) => {
     const player = useRef(null);
 
-    const closePlayer = (e)=>{
-        if(e.target === player.current){
-           setPlayState(false); 
+    const closePlayer = (e) => {
+        if (e.target === player.current) {
+            setPlayState(false);
         }
-    }
+    };
 
-  return (
-    <div className={`video-player ${playState?'':'hide'}`} ref={player} onClick={closePlayer}>
-      <video src={video} autoPlay muted controls></video>
-    </div>
-  )
-}
+    return (
+        <div
+            className={`video-player ${playState ? "" : "hide"}`}
+            ref={player}
+            onClick={closePlayer}
+        >
+            <video src={video} autoPlay muted controls></video>
+        </div>
+    );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import Hero from "../Components/Hero/Hero";
+import About from "../Components/About/About";
+import Campus from "../Components/Campus/Campus";
+import Testimonials from "../Components/Testimonials/Testimonials";
+import Contact from "../Components/Contact/Contact";
+import VideoPlayer from "../Components/VideoPlayer/VideoPlayer";
+import Title from "../Components/Title/Title";
+import ServiceSection from "../Components/Service/ServiceSection";
 
 const Home = () => {
-  return (
-    <div>
-      <h1></h1>
-    </div>
-  )
-}
+    const [playState, setPlayState] = useState(false);
 
-export default Home
+    return (
+        <>
+            <Hero />
+            <div className="container">
+                <Title subTitle="Our PRODUCTS" title="What We Offer" />
+                <Campus />
+                <About setPlayState={setPlayState} />
+                <ServiceSection /> {/* Fixed usage */}
+                <Title subTitle="" title="Our Distributership" />
+                <Testimonials />
+                <Title subTitle="Contact Us" title="Get in Touch" />
+                <Contact />
+            </div>
+            <VideoPlayer playState={playState} setPlayState={setPlayState} />
+        </>
+    );
+};
+
+export default Home;
