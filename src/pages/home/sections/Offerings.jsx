bf942cec82e96af43e27e73adcc4fd6f2dd61486
Shubham Mondal <shubhamank002@gmail.com>
2025-01-25 01:43:14 +0530

    update offerings cards

--- src/pages/home/sections/Offerings.jsx
+++ src/pages/home/sections/Offerings.jsx
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 
 const Offerings = () => {
     const [items, setItems] = useState([
-        { id: 1, image: "assets/gallery-1.jpg", name: "sample 1", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 2, image: "assets/gallery-2.jpg", name: "sample 2", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 3, image: "assets/gallery-3.jpg", name: "sample 3", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 4, image: "assets/gallery-4.jpg", name: "sample 4", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 5, image: "assets/gallery-5.jpg", name: "sample 5", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 6, image: "assets/gallery-6.jpg", name: "sample 6", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 7, image: "assets/gallery-7.jpeg", name: "sample 7", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
-        { id: 8, image: "assets/gallery-9.jpg", name: "sample 8", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 1, image: "assets/gallery-1.jpg", name: "Single Part Dispensing", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 2, image: "assets/gallery-2.jpg", name: "Two Part Dispensing", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 3, image: "assets/gallery-3.jpg", name: "Dispensing Robotos", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 4, image: "assets/gallery-4.jpg", name: "Piston Pumps", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 5, image: "assets/gallery-5.jpg", name: "Industrial Automation", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 6, image: "assets/gallery-6.jpg", name: "Pressurized Fuild Tanks", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
+        { id: 7, image: "assets/gallery-7.jpeg", name: "Pidilite Industrial Bonding Solutions", link: "/", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae." },
     ]);
 
     const [animation, setAnimation] = useState(null);
